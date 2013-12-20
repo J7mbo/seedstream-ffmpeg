e48164ffdf0f3a3eefb9974b38f599d7883ad8ba
J7mbo <jm_cj@hotmail.com>
2013-12-20 13:39:26 +0000

    Update travis settings

--- test/scripts/ffmpeg-install.sh
+++ test/scripts/ffmpeg-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# This script works great on travis ci!
+# This script works great on travis ci! (Current env is 12.04 LTS)
 
 # Append default repo
 sudo echo "deb http://archive.ubuntu.com/ubuntu precise main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
@@ -9,7 +9,7 @@ sudo echo "deb-src http://extras.ubuntu.com/ubuntu precise main" | sudo tee -a /
 # Update, upgrade etc
 sudo apt-get update && sudo apt-get upgrade -y && \
 sudo apt-get install -y autoconf automake build-essential git libass-dev libgpac-dev \
-  libmp3lame-dev libopus-dev libtheora-dev libtool libvorbis-dev libvpx-dev pkg-config texi2html \
+  libmp3lame-dev libfaac-dev libtheora-dev libtool libvorbis-dev libvpx-dev pkg-config texi2html \
   zlib1g-dev
 
 cd /usr/local/src/ && \
@@ -49,21 +49,11 @@ sudo ./configure --prefix="/usr/local/src/ffmpeg_build" --bindir="/usr/local/bin
 sudo make && sudo make install && sudo make distclean && \
 cd ..
 
-# opus
-sudo wget http://downloads.xiph.org/releases/opus/opus-1.1.tar.gz && \
-sudo tar -xvf opus-1.1.tar.gz && \
-cd opus-1.1 && \
-sudo ./configure --prefix="/usr/local/src/ffmpeg_build" --bindir="/usr/local/bin" && \
-sudo make && sudo make install && sudo make disclean && \
-cd .. && \
-sudo rm opus-1.1.tar.gz
-
-
 sudo git clone --depth 1 git://source.ffmpeg.org/ffmpeg && \
 cd ffmpeg && \
 sudo ./configure --prefix="/usr/local/src/ffmpeg_build" --extra-cflags="-I/usr/local/src/ffmpeg_build/include" \
   --extra-ldflags="-L/usr/local/src/ffmpeg_build/lib" --bindir="/usr/local/bin" --extra-libs="-ldl" --enable-gpl \
-  --enable-libfdk-aac --enable-libmp3lame --enable-libopus --enable-postproc \
+  --enable-libfdk-aac --enable-libmp3lame --enable-postproc \
   --enable-libtheora --enable-libvorbis --enable-libvpx --enable-libx264 --enable-nonfree && \
 sudo make && sudo make install && sudo make distclean && \
 hash -r && \
